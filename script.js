83776773db11f4a6c90f5647688fd1b867c5541c
AndrejYaotsin <andrej.butalov@gmx.de>
2024-04-02 13:30:34 +0200

    js/render

---
+++ script.js
@@ -0,0 +1,13 @@
+let dishName = [
+    'Schweineschnitzel "Jager Art"',
+    'Schweineschnitzel "Wiener Art"',
+    'Schweineschnitzel "Pfeffer"'
+];
+let preise = [18.90, 17.90, 18.90];
+
+
+function render() {
+    let post = document.getElementById("post_menu");
+
+    post.innerHTML = '';
+}
